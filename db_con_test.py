5043f3c6a806b26ce9a67cc8bf9c93e274d15187
il-kwang <parkik9472@gmail.com>
2025-04-03 17:53:19 +0900

    ss

--- db_con_test.py
+++ db_con_test.py
@@ -1,22 +1,27 @@
 import pymysql
 import pandas as pd
+from sqlalchemy.sql import text
+from sqlalchemy import create_engine
 
-def getdata_from_db(query):
+
+def getdata_from_db(value):
     # 데이터베이스 연결
     try:
-        with pymysql.connect(host='localhost', user='root', password='1436', 
-                             db='test', charset='utf8') as con:
-            # SQL 쿼리를 실행하고 DataFrame으로 가져오기
-            df = pd.read_sql(query, con)
-            return df
-        
+        engine = create_engine('mysql+pymysql://new_user:1234@192.168.0.187:3306/test')
+        #engine = create_engine('mysql+pymysql://root:1234@localhost/test') 내 로컬
+        with engine.begin() as con:
+            print("Database connection successful.")
+            # 데이터 삽입 쿼리 실행
+            query = text(f'INSERT INTO test.company (ID) VALUES ("{value}")')
+            #query = text(f'INSERT INTO test.com (설립연도) VALUES ("{value}")')내 로컬
+            con.execute(query)
+            print(f"Value '{value}' inserted successfully into ID.")
     except Exception as e:
-        print("An error occurred while getting data:", e)
-        return None
+        print("An error occurred while inserting data:", e)
 
 def main():
-    query = 'SELECT * FROM test.member'
-    df = getdata_from_db(query)
+    value = '박일광'
+    df = getdata_from_db(value)
     
     if df is not None:
         print(df)
--- db_insert.py
+++ db_insert.py
@@ -1,38 +1,29 @@
-import pymysql
-import csv
+import pandas as pd
+from sqlalchemy import create_engine
+
 
 def insert_to_db(csv_file_path):
     # 데이터베이스 연결
-    conn = pymysql.connect(host='127.0.0.1', user='root', password='', 
-                           db='samsung', charset='utf8')
-    try:
-        with conn.cursor() as curs:
-            # CSV 파일 열기
-            with open(csv_file_path, 'r', encoding='utf-8') as f:
-                csvReader = csv.reader(f)
+    engine = create_engine('mysql+pymysql://root:1234@localhost/test') 
 
-                # DB col : Date, Open, High, Low, Close, Adj Close, Volume
-                # 기본적으로 CSV에 헤더가 없다고 가정
-                for row in csvReader:
-                    if len(row) != 7:  # 데이터 길이가 7이 아닐 경우 건너뛰기
-                        print("Invalid row:", row)
-                        continue
-                    
-                    # 데이터 변수 할당
-                    Date, Open, High, Low, Close, AdjClose, Volume = row
-                    
-                    # SQL 쿼리 실행
-                    sql = """
-                    INSERT INTO samsung.testDB (Date, Open, High, Low, Close, AdjClose, Volume) 
-                    VALUES (%s, %s, %s, %s, %s, %s, %s)
-                    """
-                    curs.execute(sql, (Date, Open, High, Low, Close, AdjClose, Volume))
-                    print('Insert completed for date:', Date)
-                
-                # 모든 변경 사항 커밋
-                conn.commit()
+    try:
+        # CSV 파일 읽기
+        df = pd.read_csv(csv_file_path, header=None)
+        df.columns = ['ID', '설립연도', '국가', '분야', '투자단계', '직원 수', '인수여부', '상장여부', 
+                      '고객수(백만명)', '총 투자금(억원)', '연매출(억원)', 'SNS 팔로워 수(백만명)', 
+                      '기업가치(백억원)', '성공확률']
+        
+        # 데이터 삽입
+        df.to_sql(name='com', con=engine, if_exists='append', index=False)
+        print("Data inserted successfully.")
+        
+    except FileNotFoundError:
+        print(f"CSV 파일을 찾을 수 없습니다: {csv_file_path}")
     except Exception as e:
         print("An error occurred:", e)
-    finally:
-        # 데이터베이스 연결 종료
-        conn.close()
+
+
+if __name__ == '__main__':
+    # CSV 파일 경로
+    csv_file = 'C:/Users/302/Documents/미래융합교육원/DB-test/train.csv'
+    insert_to_db(csv_file)
